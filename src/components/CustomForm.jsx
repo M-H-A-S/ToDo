16e146be430d2c504b056cf0252a361e02d50dc3
Muaad Siala <muaad144301@gmail.com>
2023-08-05 16:33:06 +0100

    Input & Button

--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -14,8 +14,31 @@ const CustomForm = () => {
         >
 
         <div className="wrapper">
-            
+            <input 
+                type="text" 
+                id="task"
+                className="input"
+                // value={task}
+                // onInput={(e) => setTask(e.target.value)}
+                required
+                autoFocus
+                maxLength={60}
+                placeholder='Enter Task' 
+            />
+
+
+            <label 
+                htmlFor="task"
+                className='label'
+            >Enter Task</label>
         </div>
+        <button 
+            className="btn"
+            aria-label="Add Task"
+            type="submit"
+        >
+        <BeakerIcon className="h-6 w-6 text-blue-500" />
+        </button>
     </form>
   )
 }
