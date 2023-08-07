6ca1191808713214e5cde8104da848bb67a3e964
Muaad Siala <muaad144301@gmail.com>
2023-08-07 17:53:42 +0100

    PencilIcon Not working

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,13 +1,51 @@
+import { useState } from 'react';
 import styles from './TaskItem.module.css';
 
+// Library imports 
+import {CheckIcon} from '@heroicons/react/24/outline'
+import {PencilIcon} from '@heroicons/react/24/outline'
+
 const TaskItem = ({task}) => {
+  const [isChecked, setIsChecked] = useState(task.checked);
+
+  const handleCheckboxChange = (e) => {
+    setIsChecked(!isChecked);
+  }
+
   return (
     <li className={styles.task}>
         <div className={styles["task-group"]}>
             <input 
                 type="checkbox" 
+                className={styles.checkbox}
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+                name = {task.name}
+                id={task.id}
             />
+            <label 
+                htmlFor={task.id}
+                className={styles.label}
+            >
+              {task.name}
+              <p className={styles.checkmark}>
+                <CheckIcon strokeWidth={2} width={24}
+                height={24}/>
+              </p>
+            </label>
         </div>
+        <div className={styles["task-group"]}>
+          <button
+            className='btn'
+            aria-label={'Update ${task.name} Task'}
+            // onClick={}
+          >
+
+          </button>
+
+        </div> 
+
+
     </li>
   )
 }
