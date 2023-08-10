5f6e8cdbf2daac7b2d3977f1e87fd30532c00525
Muaad Siala <muaad144301@gmail.com>
2023-08-10 15:27:51 +0100

    closeEditMod

--- src/App.jsx
+++ src/App.jsx
@@ -68,6 +68,7 @@ function App() {
           <EditForm 
           editedTask={editedTask}
           updateTask={updateTask}
+          closeEditMode={closeEditMode}
           />
         )
       }
--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,68 +1,64 @@
+import { useState, useEffect } from 'react';
 
-import React, { useEffect } from 'react'
-import { useState,useEffect } from 'react';
- import {CheckIcon} from '@heroicons/react/24/solid';
+// library imports
+import { CheckIcon } from '@heroicons/react/24/solid'
 
+const EditForm = ({ editedTask, updateTask, closeEditMode }) => {
+  const [updatedTaskName, setUpdatedTaskName] = useState(editedTask.name);
 
-const EditForm = ({editedTask, updateTask}) => {
-    const [updatedTaskName, setUpdatedTaskName] = useState(editedTask.name);
-    // start here - 1:03:10
-
-    useEffect(() =>{
-        const closeModalIfEscaped = (e) => {
-            e.key == "Escape" && closeEditMode();
-        }
-        windowm.addEventListener('keydown', closeModalIfEscaped)
-    }, [])
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        updateTask({... editedTask, name: updatedTaskName})
-        
+  useEffect(()=> {
+    const closeModalIfEscaped = (e) => {
+      e.key === "Escape" && closeEditMode();
     }
-  
-    return (
-    <div 
-        role="dialog" 
-        aria-aria-labelledby="editTask"
-        // onClick={}
+
+    window.addEventListener('keydown', closeModalIfEscaped)
+
+    return () => {
+      window.removeEventListener('keydown', closeModalIfEscaped)
+    }
+  }, [closeEditMode])
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    updateTask({...editedTask, name: updatedTaskName})
+  }
+
+  return (
+    <div
+      role="dialog"
+      aria-labelledby="editTask"
+      onClick={(e) => {e.target === e.currentTarget && closeEditMode()}}
+      >
+      <form
+        className="todo"
+        onSubmit={handleFormSubmit}
         >
-        <form  
-            className="todo"
-            onSubmit={handleFormSubmit}
-            >
-            
-            
-            <div className="wrapper">
-                <input 
-                    type="text" 
-                    id="editTask"
-                    className="input"
-                    value={updatedTaskName}
-                    onInput={(e) => setTask(e.target.value)}
-                    required
-                    autoFocus
-                    maxLength={60}
-                    placeholder='Update Task' 
-                />
-
-
-                <label 
-                    htmlFor="task"
-                    className='label'
-                >Update Task</label>
-            </div>
-            <button 
-                className="btn"
-                aria-label={`Confirm edited task to now read ${updatedTaskName}`}
-                type="submit"
-            >
-            <   CheckIcon strokeWidth={2} height={24} width={24} />
-            
-            </button>
-        </form>
+        <div className="wrapper">
+          <input
+            type="text"
+            id="editTask"
+            className="input"
+            value={updatedTaskName}
+            onInput={(e) => setUpdatedTaskName(e.target.value)}
+            required
+            autoFocus
+            maxLength={60}
+            placeholder="Update Task"
+          />
+          <label
+            htmlFor="editTask"
+            className="label"
+          >Update Task</label>
+        </div>
+        <button
+          className="btn"
+          aria-label={`Confirm edited task to now read ${updatedTaskName}`}
+          type="submit"
+          >
+          <CheckIcon strokeWidth={2} height={24} width={24} />
+        </button>
+      </form>
     </div>
   )
 }
-
 export default EditForm
